f49a69cf328c6dabcb434a4b2c84200c3dbaa903
jimiew <jimieking@yahoo.com>
2024-02-19 20:08:35 -0500

    Delete src/main/source/umgc/mscs495/libmngntsys directory

--- src/main/source/umgc/mscs495/libmngntsys/screens/ScreenController.java
+++
@@ -1,7 +0,0 @@
-package umgc.mscs495.libmngntsys.screens;
-
-import javax.swing.JPanel;
-
-public interface ScreenController {
-	 public void show(JPanel pane);
-}
--- src/main/source/umgc/mscs495/libmngntsys/screens/members/UserInputValidationException.java
+++
@@ -1,5 +0,0 @@
-public class UserInputValidationException extends Exception {
-    public UserInputValidationException(String message) {
-        super(message);
-    }
-}
