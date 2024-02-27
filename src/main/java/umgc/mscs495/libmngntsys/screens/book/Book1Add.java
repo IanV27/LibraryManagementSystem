2644ef6aefa9a3d8c7ce97de14fd5b420b5cabc7
jimiew <jimieking@yahoo.com>
2024-02-27 16:55:54 -0500

    Add files via upload
    Jimie Wang updated for fixing bugs.

--- src/main/java/umgc/mscs495/libmngntsys/screens/book/Book1Add.java
+++ src/main/java/umgc/mscs495/libmngntsys/screens/book/Book1Add.java
@@ -4,26 +4,29 @@ package umgc.mscs495.libmngntsys.screens.book;
  * Author: Kerly LaBranche
  * School: UMGC
  * Course: CMSC 495
- * Date: 23 February 2024
+ * Date: 20 February 2024
  * Purpose: Add books to menu
  * */
+
+import javax.swing.*;
 
 import umgc.mscs495.libmngntsys.utils.AppUtils;
 import umgc.mscs495.libmngntsys.utils.JTextFieldCharLimit;
 import umgc.mscs495.libmngntsys.utils.LibLibrarianDatabaseConnection;
 import umgc.mscs495.libmngntsys.utils.ValidationUtil;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
-
 
 public class Book1Add extends JFrame {
     private final JTextField bookNameField;
@@ -31,13 +34,11 @@ public class Book1Add extends JFrame {
     private final JTextField publisherField;
     private final JTextField isbnField;
     private final JTextField barcodeField;
-    private final JComboBox subjectCombos;
-    private final JComboBox languageCombos;
-    private final JComboBox formatCombos;
-    private final JComboBox publishYrCombos;
-
-    private static final int ISBN_LENGTH = 11;
-    private static final int BARCODE_LENGTH = 10;
+    private final JComboBox subjectsCombox;
+    private final JComboBox languagesCombox;
+    private final JComboBox formatsCombox;
+    private final JComboBox publishYrCombox;
+    
     public Book1Add() {
         setTitle("Add Books");
         setSize(600, 400);
@@ -49,138 +50,122 @@ public class Book1Add extends JFrame {
         List<String> bookSubjectsLst = appUtil.getBookSubjects();
         List<String> bookLanguagesLst = appUtil.getBookLanguages();
         List<String> bookFormatsLst = appUtil.getBookFormats();
-
+        
         Font addBookFont = new Font("Serif", Font.ITALIC, 15);
         Font inputFilesFont = new Font("Serif", Font.ITALIC, 12);
 
-        bookNameField = new JTextField(15);
-        authorField = new JTextField(15);
-        publisherField = new JTextField(15);
-        isbnField = new JTextField(15);
+        bookNameField = new JTextField(20);
+        authorField = new JTextField(20);
+        publisherField = new JTextField(20);
+        isbnField = new JTextField(20);
         isbnField.setDocument(new JTextFieldCharLimit(11));
-        barcodeField = new JTextField(15);
+        barcodeField = new JTextField(20);
         barcodeField.setDocument(new JTextFieldCharLimit(10));
 
-
-        JButton addButton = new JButton("Enter");
-        addButton.addActionListener(e -> addBook());
-
+        JButton addButton = new JButton("Add");
         JButton resetButton = new JButton("Reset");
+        addButton.addActionListener(e -> addBook());
         resetButton.addActionListener(e -> clearFields());
-
+        
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(10, 2));
-
         //add book name section
-        JLabel bookNameLbl = new JLabel("   Book Name:");
-        bookNameLbl.setFont(addBookFont);
-        panel.add(bookNameLbl);
+        JLabel booknameLbl = new JLabel("   Book Name:");
+        booknameLbl.setFont(addBookFont);
+        panel.add(booknameLbl);
         bookNameField.setFont(inputFilesFont);
         panel.add(bookNameField);
-
         //add author section
         JLabel authorLbl = new JLabel("   Author:");
         authorLbl.setFont(addBookFont);
         panel.add(authorLbl);
         authorField.setFont(inputFilesFont);
         panel.add(authorField);
-
         //add publisher section
         JLabel publisherLbl = new JLabel("   Publisher:");
         publisherLbl.setFont(addBookFont);
         panel.add(publisherLbl);
         publisherField.setFont(inputFilesFont);
         panel.add(publisherField);
-
         //add isbn section
         JLabel isbnLbl = new JLabel("   ISBN:");
         isbnLbl.setFont(addBookFont);
         panel.add(isbnLbl);
         isbnField.setFont(inputFilesFont);
         panel.add(isbnField);
-
         //add barcode section
         JLabel barcodeLbl = new JLabel("   Barcode:");
         barcodeLbl.setFont(addBookFont);
         panel.add(barcodeLbl);
         barcodeField.setFont(inputFilesFont);
-
         //add subject section
         panel.add(barcodeField);
         JLabel subjectLbl = new JLabel("   Subject:");
         subjectLbl.setFont(addBookFont);
         panel.add(subjectLbl);
         String[] subjectsArr = bookSubjectsLst.toArray(String[]::new);
-        subjectCombos = new JComboBox(subjectsArr);
-        subjectCombos.setFont(addBookFont);
-        panel.add(subjectCombos);
-
+        subjectsCombox = new JComboBox(subjectsArr);
+        subjectsCombox.setFont(addBookFont);
+        panel.add(subjectsCombox);
         //add language section
         JLabel langLbl = new JLabel("   Language:");
         langLbl.setFont(addBookFont);
         panel.add(langLbl);
         String[] languagesArr = bookLanguagesLst.toArray(String[]::new);
-        languageCombos = new JComboBox(languagesArr);
-        languageCombos.setFont(addBookFont);
-        panel.add(languageCombos);
-
+        languagesCombox = new JComboBox(languagesArr);
+        languagesCombox.setFont(addBookFont);
+        panel.add(languagesCombox);
         //add book format section
         JLabel formatLbl = new JLabel("   Format:");
         formatLbl.setFont(addBookFont);
         panel.add(formatLbl);
         String[] formatsArr = bookFormatsLst.toArray(String[]::new);
-        formatCombos = new JComboBox(formatsArr);
-        formatCombos.setFont(addBookFont);
-        panel.add(formatCombos);
-
+        formatsCombox = new JComboBox(formatsArr);
+        formatsCombox.setFont(addBookFont);
+        panel.add(formatsCombox);
         //add publish year section
         JLabel publishYrLbl = new JLabel("   Publisher Year: ");
         publishYrLbl.setFont(addBookFont);
         panel.add(publishYrLbl);
         Calendar now = Calendar.getInstance(Locale.US);
-        String[] publishYearArr = new String[now.get(Calendar.YEAR) - 1919];
+        String publishYearArr[] = new String[now.get(Calendar.YEAR) - 1919];
         publishYearArr[0] = "Select Year";
         for(int year=now.get(Calendar.YEAR); year > 1920; year--) {
-            publishYearArr[now.get(Calendar.YEAR) - year + 1] ="" + year;
+        	publishYearArr[now.get(Calendar.YEAR) - year + 1] = "" + year;
         }
-        publishYrCombos = new JComboBox(publishYearArr);
-        publishYrCombos.setFont(addBookFont);
-        panel.add(publishYrCombos);
+        publishYrCombox = new JComboBox(publishYearArr);
+        publishYrCombox.setFont(addBookFont);
+        panel.add(publishYrCombox);
         addButton.setFont(addBookFont);
         resetButton.setFont(addBookFont);
         panel.add(addButton);
         panel.add(resetButton);
 
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        buttonPanel.add(addButton);
-
-        add(panel, BorderLayout.CENTER);
-        add(buttonPanel, BorderLayout.SOUTH);
+        add(panel);        
     }
-
-    //Adding book information into db
+    
+    /**
+     * Adding book information into db
+     */
     private void addBook() {
         String bookName = bookNameField.getText();
         String author = authorField.getText();
         String publisher = publisherField.getText();
         String isbn = isbnField.getText();
         String barcode = barcodeField.getText();
-        String subject = (String) subjectCombos.getSelectedItem();
-        String language = (String) languageCombos.getSelectedItem();
-        String format = (String) formatCombos.getSelectedItem();
-        String publishYear = (String) publishYrCombos.getSelectedItem();
+        String subject = (String)subjectsCombox.getSelectedItem();
+        String language = (String)languagesCombox.getSelectedItem();
+        String format = (String)formatsCombox.getSelectedItem();
+        String publishYear = (String)publishYrCombox.getSelectedItem();
         ValidationUtil valUtil = new ValidationUtil();
         int publishYearInt = 0;
         if(valUtil.isDigits(publishYear)) {
-            assert publishYear != null;
-            publishYearInt = Integer.parseInt(publishYear);
+        	publishYearInt = Integer.parseInt(publishYear);
         }
         System.out.println(publishYearInt);
-
         // Validate all fields
         if (bookName.isEmpty() || author.isEmpty() || publisher.isEmpty() || isbn.isEmpty() || barcode.isEmpty() ||
-                Objects.requireNonNull(subject).isEmpty() || Objects.requireNonNull(language).isEmpty() ||
-                Objects.requireNonNull(format).isEmpty() || Objects.requireNonNull(publishYear).isEmpty()) {
+                subject.isEmpty() || language.isEmpty() || format.isEmpty()) {
             JOptionPane.showMessageDialog(this, "Please fill in all fields.");
             return;
         }
@@ -203,74 +188,119 @@ public class Book1Add extends JFrame {
         clearFields();
     }
 
+    /**
+     * Add book into database tables.
+     * @param bookName
+     * @param author
+     * @param publisher
+     * @param isbn
+     * @param barcode
+     * @param subject
+     * @param language
+     * @param format
+     * @param pubYear
+     */
     private void addBookToDatabase(String bookName, String author, String publisher, String isbn, String barcode,
                                    String subject, String language, String format, int pubYear) {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/library",
-                    "root", "");
-                    connection = LibLibrarianDatabaseConnection.getConnection();
-            String sql = "INSERT INTO books (Title, ISBN, Author, Publisher, Subject, Language," +
-                    "format, PublishYear) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-
-            statement = connection.prepareStatement(sql);
-            statement.setString(1, bookName);
-            statement.setString(2, isbn);
-            statement.setString(3, author);
-            statement.setString(4, publisher);
-            statement.setString(5, subject);
-            statement.setString(6, language);
-            statement.setString(7, format);
-            statement.setInt(8, pubYear);
-
-            int rowsInserted = statement.executeUpdate();
-            if (rowsInserted > 0) {
-                JOptionPane.showMessageDialog(this, "Book added successfully.");
-            } else {
-                JOptionPane.showMessageDialog(this, "Failed to add book.");
-            }
-
-            sql = "insert into singlebook(isbn, barcode,reserved) values(?, ?, 0)";
-
-            statement = connection.prepareStatement(sql);
+    	Connection connection = null;
+    	PreparedStatement statement = null;
+    	ResultSet rs = null;
+    	try {
+//            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/library",
+//                    "root", "");
+        	connection = LibLibrarianDatabaseConnection.getConnection();
+        	String query = "SELECT * FROM books WHERE ISBN = ?";
+        	boolean isbnAlreadyExisted = false;
+        	boolean barcodeAlreadyExisted = false;
+        	String sql = "";
+            statement = connection.prepareStatement(query);
             statement.setString(1, isbn);
-            statement.setString(2, barcode);
-
-            rowsInserted = statement.executeUpdate();
-            if (rowsInserted > 0) {
-                JOptionPane.showMessageDialog(this, "Single Book added successfully.");
-            } else {
-                JOptionPane.showMessageDialog(this, "Failed to add Single book.");
+            rs = statement.executeQuery();
+            while(rs.next()) {
+            	isbnAlreadyExisted = true;
             }
-
+            int rowsInserted = -1;
+        	if(!isbnAlreadyExisted) {
+	            sql = "INSERT INTO books (Title, ISBN, Author, Publisher, Subject, Language," +
+	                    " format, PublishYear) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+	            
+	            statement = connection.prepareStatement(sql);
+	            statement.setString(1, bookName);
+	            statement.setString(2, isbn);
+	            statement.setString(3, author);
+	            statement.setString(4, publisher);
+	            statement.setString(5, subject);
+	            statement.setString(6, language);
+	            statement.setString(7, format);
+	            statement.setInt(8, pubYear);
+	
+	            rowsInserted = statement.executeUpdate();
+	            if (rowsInserted > 0) {
+	                JOptionPane.showMessageDialog(this, "Book added successfully.");
+	            } else {
+	                JOptionPane.showMessageDialog(this, "Failed to add book.");
+	            }
+        	}
+        	query = "SELECT * FROM singlebook WHERE barcode = ?";
+            statement = connection.prepareStatement(query);
+            statement.setString(1, barcode);
+            rs = statement.executeQuery();
+            while(rs.next()) {
+            	barcodeAlreadyExisted = true;
+            }
+            if(!barcodeAlreadyExisted) {
+	            sql = "insert into singlebook(isbn, barcode,reserved) values(?, ?, 0)";
+	
+	            statement = connection.prepareStatement(sql);
+	            statement.setString(1, isbn);
+	            statement.setString(2, barcode);
+	
+	            rowsInserted = statement.executeUpdate();
+	            if (rowsInserted > 0) {
+	                JOptionPane.showMessageDialog(this, "Single Book added successfully.");
+	            } else {
+	                JOptionPane.showMessageDialog(this, "Failed to add Single book.");
+	            }
+            } else {
+                JOptionPane.showMessageDialog(this, "Failed to add Single book, barcode already is in use.");
+            }
+            
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
             JOptionPane.showMessageDialog(this, "Error adding book to the database.");
         } finally {
-            try {
-                if(statement != null) {
-                    statement.close();
-                }
-                if(connection != null) {
-                    connection.close();
-                }
-            } catch(SQLException e) {
-                e.printStackTrace();
-            }
+        	try {
+        		if(statement != null) {
+        			statement.close();
+        		}
+        		if(connection != null) {
+        			connection.close();
+        		}
+        	} catch(Exception e) {
+        		e.printStackTrace();
+        	}
         }
     }
-
-    //validate ISBN
+    
+    /**
+     * Validate input isbn
+     * @param isbn
+     * @return
+     */
     private boolean validateISBN(String isbn) {
         // Trim whitespace and validate ISBN format
-        return isbn.trim().matches("\\d{" + ISBN_LENGTH + "}");
+        return isbn.trim().matches("\\d{11}");
     }
 
+    /**
+     * validate input barcode
+     * @param barcode
+     * @return
+     */
     private boolean validateBarcode(String barcode) {
         // Validate barcode format
-        return barcode.matches("\\d{" + BARCODE_LENGTH + "}");
+        return barcode.matches("\\d{10}");
     }
 
     private void clearFields() {
@@ -279,13 +309,13 @@ public class Book1Add extends JFrame {
         publisherField.setText("");
         isbnField.setText("");
         barcodeField.setText("");
-        subjectCombos.setSelectedIndex(0);
-        languageCombos.setSelectedIndex(0);
-        formatCombos.setSelectedIndex(0);
-        publishYrCombos.setSelectedIndex(0);
+        subjectsCombox.setSelectedIndex(0);
+        languagesCombox.setSelectedIndex(0);
+        formatsCombox.setSelectedIndex(0);
+        publishYrCombox.setSelectedIndex(0);
     }
 
     public static void main(String[] args) {
-        new Book1Add().setVisible(true);
+        new AddBooksMenu().setVisible(true);
     }
 }
