26e22a5e7e64a037f34a5fd22ed8bbde253577e9
jimiew <jimieking@yahoo.com>
2024-02-27 16:21:14 -0500

    Add files via upload
    Jimie Wang updated for fixing compiling issue.

--- src/main/java/umgc/mscs495/libmngntsys/utils/AppLoggingUtil.java
+++ src/main/java/umgc/mscs495/libmngntsys/utils/AppLoggingUtil.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
